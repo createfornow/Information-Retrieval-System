14728940f15cad5c11a8408fd68e800975183d69
createfornow <create.raviyadav@gmail.com>
2025-02-22 15:18:16 +0530

    Version 1

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'GenAI Project',
+    version = '0.0.0',
+    author = 'Ravi',
+    author_email = 'create.raviyadav@gmail.com',
+    packages = find_packages(),
+    instal_requires = []
+)
